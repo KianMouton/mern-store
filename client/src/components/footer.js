fc84078825177c5dfa92da3de1ae06bc85da78aa
KianMouton <kianton2004@gmail.com>
2024-08-13 15:42:19 +0200

    added links to the footer

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -1,9 +1,17 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 
 function Footer() {
     return (
         <div className="footer">
-            <p>this is the footer</p>
+            <p>follow me on my socials</p>
+            <div className='icons'>   
+             <a href="https://www.facebook.com/profile.php?id=100078638864939">  
+             <FontAwesomeIcon icon={faFacebook} size='2x' />
+             </a>
+             <FontAwesomeIcon icon={faInstagram} size='2x' />
+            </div>
         </div>
     )
 }
